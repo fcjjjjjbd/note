f66ac1dbb27ba00387b870c198f1f2c10d006975
fcjjjjjbd <162014680+fcjjjjjbd@users.noreply.github.com>
2025-10-31 20:36:27 +0800

    a

---
+++ pages_fen数据管理/useradv对用户广告/useradv.vue
@@ -0,0 +1,23 @@
+<!-- 行止至善1 {知}1力行动做到1生 stay错1改1 转0为1 不1则0 以大1兼小 舍小1就大 抱1负0,好习惯 -->
+<script setup>
+  const demo = ref(null)
+
+  const demoo = async () => {
+
+  }
+  console.log()
+</script>
+<template>
+  <view class="">
+    {"_id":{"$oid":"6776812cc3b5c934eb57701a"},"state":true,"name":"有订单后，选择师傅后电话先联系,问清楚详细情况，材料费，价格相关费用谈好而后上门，","sort":3}
+    {"_id":{"$oid":"6776df5d55b337b42b786a12"},"state":true,"name":"微信小程序正在上线中,敬请期待","sort":2}
+    {"_id":{"$oid":"6776df8b9755e37462b53da5"},"state":true,"name":"任何矛盾,问题 加客服微信13453555442","sort":0}
+    {"_id":{"$oid":"67a72dd5f08210ad57d7eba0"},"state":true,"name":"上门不收取任何费用,免费上门","sort":1}
+    {"_id":{"$oid":"67b15588816a3ff63bb6184b"},"state":true,"name":"每个商家广告，要具体写干什么的，每个明码标价,没有写具体价格的,平台发现会被删除的","sort":9}
+
+  </view>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- uniCloud-aliyun/cloudfunctions/secCheckContent-文字图片校验/new_file.vue
+++ uniCloud-aliyun/cloudfunctions/secCheckContent-文字图片校验/new_file.vue
@@ -2,20 +2,28 @@
 <script setup>
   openid: "o5tQd7U__aPjwWnoYBPUivS4C_sw"
   需要关联2个文件
-  const secCheckObj = uniCloud.importObject("secCheckContent");
 
   const onSubmit = async () => {
-    uni.showLoading()
-    let secRes = await secCheckObj.textSecCheck(commentData.value.content, userStore.userInfo.openid);
-    if (secRes.code) {
-      uni.hideLoading()
+    const secCheckObj = uniCloud.importObject("secCheckContent");
+
+    uni.showLoading();
+    let secRes = await secCheckObj.textSecCheck(
+      dataobj.value.content,
+      "o5tQd7U__aPjwWnoYBPUivS4C_sw"
+    );
+    console.log(secRes);
+    if (secRes.code == 0) {
+      goodsff();
+    } else {
+      uni.hideLoading();
       uni.showModal({
-        title: secRes.errMsg,
-        content: `发布内容存在"${secRes.result.label}"问题,请重新编辑后发布!`,
-        showCancel: false
-      })
+        title: secRes.errMsg || '操作失败',
+        content: secRes.result?.label ? `发布内容存在"${secRes.result.label}"问题,请重新编辑后发布!` : '请检查输入内容后重试',
+        showCancel: false,
+      });
       return;
     }
+
   }
   const demoo = async () => {
 
