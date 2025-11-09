0a376e90e3f9609ef63a1e6c80545f627448e696
fcjjjjjbd <162014680+fcjjjjjbd@users.noreply.github.com>
2025-11-09 21:35:24 +0800

    a

--- 编程工具/github.vue
+++ 编程工具/github.vue
@@ -1,17 +1,17 @@
 <!-- 行止至善1 {知}1力行动做到1生 stay错1改1 转0为1 不1则0 以大1兼小 舍小1就大 抱1负0,好习惯 -->
 <script setup>
-  const demo = ref(null)
-
-  const demoo = async () => {
-
-  }
-  console.log()
+  苹果商店前端
+  https: //pan.baidu.com/s/1U97CAN4e3uZXjrVz_tjjdA?pwd=94um
+    度盘
+  https: //drive.google.com/file/d/1Pw5HIxf20guQS2YamHcYosEGDnNfLcCl/view?usp=drive_link
+    谷歌网盘
+  哔哩哩后端
+  https: //github.com/p4gefau1t/openbilibili.git
 </script>
 <template>
   <view class="">
     github.com换zread.ai
-    下载应用
-    查配置win+r msinfo32搜索是否x64
+
   </view>
 </template>
 
