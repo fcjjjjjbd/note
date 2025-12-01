f24d96cf433cc6efa5a0de447da9e483a105c7f1
fcjjjjjbd <162014680+fcjjjjjbd@users.noreply.github.com>
2025-12-01 23:33:42 +0800

    a

--- ai编程使用/mcp.vue
+++ ai编程使用/mcp.vue
@@ -1,3 +1,22 @@
-github操作git
+命令行
+claude  mcp list
+chromedevtools
 contenxt7和browsermcp，可以加快网站类产品的开发效率，
 context7 可以引入最新的代码库知识，而browsermcp，它可以让你的 cc/trae 直接打开你的浏览器去查看内容！
+cc配置飞书mcp
+npx -y @larksuiteoapi/lark-mcp login -a cli_a9a653a3eefbdcd3 -s hnEG5PpK4jmoC7iNdTGaubkCCogP14TM
+
+claude mcp add-json --scope=user lark-mcp '{
+"command": "npx",
+"args": [
+"-y",
+"@larksuiteoapi/lark-mcp",
+"mcp",
+"-a",
+"cli_a9a653a3eefbdcd3",
+"-s",
+"hnEG5PpK4jmoC7iNdTGaubkCCogP14TM",
+"--oauth"
+]
+}'
+
---
+++ a工具聚合/filmora剪辑.vue
@@ -0,0 +1,20 @@
+<!-- 行止至善1 {知}1力行动做到1生 stay错1改1 转0为1 不1则0 以大1兼小 舍小1就大 抱1负0,好习惯 -->
+<script setup>
+  const demo = ref(null)
+
+  const demoo = async () => {
+
+  }
+  console.log()
+</script>
+<template>
+  <view class="">
+    TV老张剪辑
+    我的电脑位置C:\Users\admin\AppData\Roaming\Wondershare\Wondershare Filmora\Output
+    音频先下载试听后导入
+  </view>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
