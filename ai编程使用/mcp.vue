46bca61a4813cc0721675d07148c8f129fe26dd1
fcjjjjjbd <162014680+fcjjjjjbd@users.noreply.github.com>
2025-12-08 01:24:49 +0800

    sa

--- ai编程使用/mcp.vue
+++ ai编程使用/mcp.vue
@@ -1,22 +1,12 @@
 命令行
 claude  mcp list
+claude  mcp add git   --scope user  --  npx -y@modelcontextprotocol/server-git
 chromedevtools
-contenxt7和browsermcp，可以加快网站类产品的开发效率，
-context7 可以引入最新的代码库知识，而browsermcp，它可以让你的 cc/trae 直接打开你的浏览器去查看内容！
-cc配置飞书mcp
+playwright  浏览器自动化
+contenxt7   获取实时官方文档
+equential Thinking深度思考任务分解
+browsermcp浏览器去查看内容！
+lark-mcp飞书
 npx -y @larksuiteoapi/lark-mcp login -a cli_a9a653a3eefbdcd3 -s hnEG5PpK4jmoC7iNdTGaubkCCogP14TM
-
-claude mcp add-json --scope=user lark-mcp '{
-"command": "npx",
-"args": [
-"-y",
-"@larksuiteoapi/lark-mcp",
-"mcp",
-"-a",
-"cli_a9a653a3eefbdcd3",
-"-s",
-"hnEG5PpK4jmoC7iNdTGaubkCCogP14TM",
-"--oauth"
-]
-}'
-
+brave   search  实时网络搜索
+sentyr错误监控
