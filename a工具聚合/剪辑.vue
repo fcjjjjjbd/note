4b2752899513f67a25ac79a083380eed5fca7b6a
fcjjjjjbd <162014680+fcjjjjjbd@users.noreply.github.com>
2025-12-07 12:34:35 +0800

    测试云端

---
+++ a工具聚合/剪辑.vue
@@ -0,0 +1,21 @@
+<!-- 行止至善1 {知}1力行动做到1生 stay错1改1 转0为1 不1则0 以大1兼小 舍小1就大 抱1负0,好习惯 -->
+<script setup>
+  const demo = ref(null)
+
+  const demoo = async () => {
+
+  }
+  console.log()
+</script>
+<template>
+  <view class="">
+    filmora
+    我的电脑位置C:\Users\admin\AppData\Roaming\Wondershare\Wondershare Filmora\Output
+    音频先下载试听后导入
+    效果/影片/模糊(拖视频里)
+  </view>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
