36f77bc9a15dabed64ec3e331f4f75f77d2dcfd5
fcjjjjjbd <162014680+fcjjjjjbd@users.noreply.github.com>
2025-12-14 04:55:40 +0800

    1

--- ai编程使用/claudecode笔记.vue
+++ ai编程使用/claudecode笔记.vue
@@ -1,6 +1,3 @@
-
-
-
 mcp连接操纵外部工具,git,jql,设计表结构,添加模拟数据,调用API,自动测试
 视频:抓蛙师3-4.10抄claude.md
 网址
@@ -12,12 +9,14 @@ ai不是全正确的也会犯错,过于依赖
 CLAUDE.md,项目规范代码风格业务逻辑,ai响应质量的核心,不断打磨
 功能
 figma转前端,图片提问
-plan   mode启动交互式问答  
- think hard,think harder,ultrathink提高上下文思考长度
-  claude mcp list查看列表
- /init初始化项目生成CLAUDE.md
- /clear清除上下文历史记录
- /compact浓缩上下文,保留xx功能
+claude --help快捷键帮助指导
+claude --permission-mode=plan
+plan mode启动交互式问答
+think hard,think harder,ultrathink提高上下文思考长度
+claude mcp list查看列表
+/init初始化项目生成CLAUDE.md
+/clear清除上下文历史记录
+/compact浓缩上下文,保留xx功能
 /resume切换回上下文
 /rewind切换择历史记录指令
 提示词
@@ -26,10 +25,5 @@ plan   mode启动交互式问答
 
 快捷键
 esc停止输出
-!把对话窗口切换命令行
 /clear清除历史记录
-shift,win,s截屏
-cc启动脚本system32/cc.bat @echo   off Claude --dangerously-skip-permissions   %*
-
-
- 
+cc启动脚本system32/cc.bat @echo off Claude --dangerously-skip-permissions %*
